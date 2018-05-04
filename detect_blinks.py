a188e0cf676cf85e44a42839ff4bbca3effdbeb4
Mans <mans.mmen@qq.com>
2018-05-04 16:55:54 +0800

    Update detect_blinks.py

--- detect_blinks.py
+++ detect_blinks.py
@@ -1,6 +1,4 @@
-# USAGE
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
 
 # import the necessary packages
 from scipy.spatial import distance as dist
